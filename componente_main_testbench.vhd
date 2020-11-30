463d1d60bab09e2b4d3f72a98c586dc8f54e9500
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2020-11-30 09:21:41 -0300

    Update main testbench

--- componente_main_testbench.vhd
+++ componente_main_testbench.vhd
@@ -12,14 +12,27 @@ begin
         port map(sys_clk => test_sys_clk, interval => test_interval, out_limp => test_limp, out_hab => test_hab, out_arm => test_arm);
     clock_process :process
         begin
-            test_sys_clk <= '0';
-            wait for 5 us;
-            test_sys_clk <= '1';
-            wait for 5 us;
-    end process;
-    stim_proc: process
-        begin        
-        test_interval <= "000";
-    wait;
+            test_interval <= "000";
+            loop_1 : for k in 0 to 5 loop
+                test_sys_clk <= '1';
+                wait for 5 us;
+                test_sys_clk <= '0';
+                wait for 5 us;
+            end loop loop_1;
+            test_interval <= "001";
+            loop_2 : for k in 0 to 60 loop
+                test_sys_clk <= '1';
+                wait for 5 us;
+                test_sys_clk <= '0';
+                wait for 5 us;
+            end loop loop_2;
+            test_interval <= "010";
+            loop_3 : for k in 0 to 500 loop
+                test_sys_clk <= '1';
+                wait for 5 us;
+                test_sys_clk <= '0';
+                wait for 5 us;
+            end loop loop_3;
+            wait;
     end process;
 end arch;
